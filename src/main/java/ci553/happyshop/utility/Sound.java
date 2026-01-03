c7b4f5472a068c8c42d9085c4df3aa97f37df2fa
Moaz1213 <m.mohamed16@uni.brighton.ac.uk>
2026-01-03 05:14:18 +0000

    Add Beep Sound to the buttons

---
+++ src/main/java/ci553/happyshop/utility/Sound.java
@@ -0,0 +1,10 @@
+package ci553.happyshop.utility;
+
+import java.awt.Toolkit;
+
+public class Sound {
+
+    public static void playClickSound() {
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
