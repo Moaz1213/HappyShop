2eb0e38a0633bf229042f1caca1506596ff3e3d7
Moaz1213 <m.mohamed16@uni.brighton.ac.uk>
2026-01-03 05:28:17 +0000

    Add Beep Sound to the buttons

--- src/main/java/ci553/happyshop/client/customer/CustomerView.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerView.java
@@ -218,3 +218,4 @@ public class CustomerView  {
                   viewWindow.getWidth(), viewWindow.getHeight());
     }
 }
+
--- src/main/java/ci553/happyshop/utility/Sound.java
+++ src/main/java/ci553/happyshop/utility/Sound.java
@@ -8,3 +8,4 @@ public class Sound {
         Toolkit.getDefaultToolkit().beep();
     }
 }
+
